6d1ad9d632a15fc9a4680239d5bd3e8a5ad9edef
jseursing <jseursing@gmail.com>
2021-09-18 12:00:20 -0700

    Removed VM related code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
 #include "SMT.h"
-#include "VMUtils.h"
 #include <QtWidgets/QApplication>
-
-// Forward declarations
-extern void InitializeVM();
-extern void ValidateUIDCallback();
-extern void VMLCRC32Callback();
-extern void VMLHBCallback();
 
 //
 // Entrypoint
 //
 int main(int argc, char *argv[])
 {
-  InitializeVM();
-  ValidateUIDCallback();
-  VMLCRC32Callback();
-
   QApplication a(argc, argv);
   SMT w(0, ValidateUIDCallback, VMLCRC32Callback, VMLHBCallback);
   w.show();
